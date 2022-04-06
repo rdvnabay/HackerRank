a2929e0545c23d069792dfaeea528296faf05bb4
rdvnabay <ridvanabay@gmail.com>
2022-04-06 13:15:18 +0300

    Solution 9: Time Conversion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Algorithms;
 
 //Lesson 8: BirthdayCakeCandles
-List<int> inputs = new() { 3, 2, 1, 3 };
-int output = BirthdayCakeCandles.Run(inputs); //output: 2
+List<int> input_8 = new() { 3, 2, 1, 3 };
+int output_8 = BirthdayCakeCandles.Run(input_8); //output: 2
+
+//Lesson 9: TimeConversion
+string input_9 = "07:05:45PM";
+string output_9 = TimeConversion.Run(input_9); //output: 19:05:45
