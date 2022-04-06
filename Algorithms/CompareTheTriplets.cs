435f10f56ba5c73d47deb33abd4bb2f2bcc1f579
rdvnabay <ridvanabay@gmail.com>
2022-04-06 13:35:23 +0300

    Solution 3: Compare the Triplets

---
+++ Algorithms/CompareTheTriplets.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms;
+
+public class CompareTheTriplets
+{
+    public static List<int> Run(List<int> a, List<int> b)
+    {
+        var list = new List<int>();
+        int scoreA = 0;
+        int scoreB = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (a[i] > b[i])
+                scoreA++;
+            else if (a[i] < b[i])
+                scoreB++;
+        }
+
+        list.Add(scoreA);
+        list.Add(scoreB);
+        return list;
+    }
+}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,8 +3,12 @@
 //Lesson 1: SolveMeFirst
 int output_1 = SolveMeFirst.Run(3, 5); //output: 8
 
-//Lesson 1: SimpleArraySum
+//Lesson 2: SimpleArraySum
 int output_2 = SimpleArraySum.Run(new() { 1, 2, 3, 4, 10, 11 }); //output: 31
+
+//Lesson 3: CompareTheTriplets
+List<int> output_3 = CompareTheTriplets.Run(new() { 5, 6, 7 }, new() { 3, 6, 10 }); //output: 1 1
+Console.WriteLine(String.Join(" ", output_3));
 
 //Lesson 8: BirthdayCakeCandles
 int output_8 = BirthdayCakeCandles.Run(new() { 3, 2, 1, 3 }); //output: 2
