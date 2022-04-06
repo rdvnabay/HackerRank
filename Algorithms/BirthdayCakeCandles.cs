12bd9ef017236ee7b834e28d1e2f5e813c7e5be0
rdvnabay <ridvanabay@gmail.com>
2022-04-06 13:07:18 +0300

    Solution 8: BirthdayCakeCandles

---
+++ Algorithms/BirthdayCakeCandles.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorithms;
+
+public class BirthdayCakeCandles
+{
+    //Define: You are in charge of the cake for a child's birthday.
+    //You have decided the cake will have one candle for each year of their total age.
+    //They will only be able to blow out the tallest of the candles. Count how many candles are tallest. 
+
+    //Solution
+    public static int Run(List<int> candles)
+    {
+        int maxValue = candles.MaxBy(x => x);
+        int result = candles.Where(x => x == maxValue).Count();
+        return result;
+    }
+}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using Algorithms;
+
+//Lesson 8: BirthdayCakeCandles
+List<int> inputs = new() { 3, 2, 1, 3 };
+int output = BirthdayCakeCandles.Run(inputs); //output: 2
