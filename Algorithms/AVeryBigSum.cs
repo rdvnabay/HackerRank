277261f449635cb41a780d9ffdf2ff03a86763c5
rdvnabay <ridvanabay@gmail.com>
2022-04-06 14:00:42 +0300

    Solution 4: A Very Big Sum

---
+++ Algorithms/AVeryBigSum.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms;
+public class AVeryBigSum
+{
+    /*
+     In this challenge, you are required to calculate and print the sum of the elements in an array,
+     keeping in mind that some of those integers may be quite large.
+     */
+    public static long Run(List<long> ar)
+    {
+        long total = 0;
+        foreach (var val in ar)
+        {
+            total += val;
+        }
+        return total;
+    }
+}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,21 @@
 ﻿using Algorithms;
 
-//Lesson 1: SolveMeFirst
+//Lesson 1: Solve Me First
 int output_1 = SolveMeFirst.Run(3, 5); //output: 8
 
-//Lesson 2: SimpleArraySum
+//Lesson 2: Simple Array Sum
 int output_2 = SimpleArraySum.Run(new() { 1, 2, 3, 4, 10, 11 }); //output: 31
 
-//Lesson 3: CompareTheTriplets
+//Lesson 3: Compare The Triplets
 List<int> output_3 = CompareTheTriplets.Run(new() { 5, 6, 7 }, new() { 3, 6, 10 }); //output: 1 1
 Console.WriteLine(String.Join(" ", output_3));
 
-//Lesson 8: BirthdayCakeCandles
+//Lesson 4: A Very Big Sum
+long output_4 = AVeryBigSum.Run(new() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }); //output: 5000000015
+
+//Lesson 8: Birthday Cake Candles
 int output_8 = BirthdayCakeCandles.Run(new() { 3, 2, 1, 3 }); //output: 2
 
-//Lesson 9: TimeConversion
+//Lesson 9: Time Conversion
 string output_9 = TimeConversion.Run("07:05:45PM"); //output: 19:05:45
 
