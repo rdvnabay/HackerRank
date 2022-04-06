b17ff9b9f318928b13352ab19e86d60f224ae978
rdvnabay <ridvanabay@gmail.com>
2022-04-06 17:11:28 +0300

    Solution 10:  Grading Students

---
+++ Algorithms/GradingStudents.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms;
+
+public class GradingStudents
+{
+    public static List<int> Run(List<int> grades)
+    {
+        int roundedNote = 0;
+        int increaseNote = 0;
+        for (int i = 0; i < grades.Count; i++)
+        {
+            if (grades[i] <= 35)
+                continue;
+
+            roundedNote = 5 * (int)Math.Round(grades[i] / 5.0);
+            if (grades[i] < roundedNote)
+            {
+                increaseNote = roundedNote - grades[i];
+                grades[i] += increaseNote;
+            }
+        }
+        return grades;
+    }
+}
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -21,14 +21,20 @@ input_5.Add(new List<int> { 10, 8, -12 });
 int output_5 = DiagonalDifference.Run(input_5); //output: 15
 
 //Lesson 6: Plus Minus
-PlusMinus.Run(new() { -4, 3, -9, 0, 4, 1 });
+PlusMinus.Run(new() { -4, 3, -9, 0, 4, 1 }); //output: 0,5 0,33.. 0,16..
 
 //Lesson 7: Mini-Max Sum
-MiniMaxSum.Run(new() { 1, 2, 3, 4, 5 });
+MiniMaxSum.Run(new() { 1, 2, 3, 4, 5 }); //output: 10 14
 
 //Lesson 8: Birthday Cake Candles
 int output_8 = BirthdayCakeCandles.Run(new() { 3, 2, 1, 3 }); //output: 2
 
 //Lesson 9: Time Conversion
 string output_9 = TimeConversion.Run("07:05:45PM"); //output: 19:05:45
+
+//Lesson 10: Grading Students
+List<int> output_10 = GradingStudents.Run(new() { 73, 67, 38, 33 });
+output_10.ForEach((item => Console.WriteLine(item)));
+
+
 
