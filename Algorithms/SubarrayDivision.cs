4bf523491d321b3fe21bb3bb7a6284b4930f7611
rdvnabay <ridvanabay@gmail.com>
2022-04-07 18:25:31 +0300

    Solution 15: Subarray Division

---
+++ Algorithms/SubarrayDivision.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms;
+
+public class SubarrayDivision
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s"> [1 2 1 3 2] </param>
+    /// <param name="d"> 3 </param>
+    /// <param name="m"> 2 </param>
+    /// <returns></returns>
+    /// 
+    public static int Run(List<int> s, int d, int m)
+    {
+        int startRange = 0;
+        int endRange = m - 1;
+        int counter = 0;
+        int total = 0;
+
+        for (int i = 0; i < s.Count; i++)
+        {
+            total += s[i];
+            if (i == endRange)
+            {
+                if (total == d)
+                    counter++;
+
+                startRange++;
+                endRange++;
+                i = startRange - 1;
+                total = 0;           
+            }
+        }
+        return counter;
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,50 +1,54 @@
 ﻿using Algorithms;
 
-//Lesson 1: Solve Me First
+//Chapter 1: Solve Me First
 int output_1 = SolveMeFirst.Run(3, 5); //output: 8
 
-//Lesson 2: Simple Array Sum
+//Chapter 2: Simple Array Sum
 int output_2 = SimpleArraySum.Run(new() { 1, 2, 3, 4, 10, 11 }); //output: 31
 
-//Lesson 3: Compare The Triplets
+//Chapter 3: Compare The Triplets
 List<int> output_3 = CompareTheTriplets.Run(new() { 5, 6, 7 }, new() { 3, 6, 10 }); //output: 1 1
 Console.WriteLine(String.Join(" ", output_3));
 
-//Lesson 4: A Very Big Sum
+//Chapter 4: A Very Big Sum
 long output_4 = AVeryBigSum.Run(new() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }); //output: 5000000015
 
-//Lesson 5: Diagonal Difference
+//Chapter 5: Diagonal Difference
 List<List<int>> input_5 = new();
 input_5.Add(new List<int> { 11, 2, 4 });
 input_5.Add(new List<int> { 4, 5, 6 });
 input_5.Add(new List<int> { 10, 8, -12 });
 int output_5 = DiagonalDifference.Run(input_5); //output: 15
 
-//Lesson 6: Plus Minus
+//Chapter 6: Plus Minus
 PlusMinus.Run(new() { -4, 3, -9, 0, 4, 1 }); //output: 0,5 0,33.. 0,16..
 
-//Lesson 7: Mini-Max Sum
+//Chapter 7: Mini-Max Sum
 MiniMaxSum.Run(new() { 1, 2, 3, 4, 5 }); //output: 10 14
 
-//Lesson 8: Birthday Cake Candles
+//Chapter 8: Birthday Cake Candles
 int output_8 = BirthdayCakeCandles.Run(new() { 3, 2, 1, 3 }); //output: 2
 
-//Lesson 9: Time Conversion
+//Chapter 9: Time Conversion
 string output_9 = TimeConversion.Run("07:05:45PM"); //output: 19:05:45
 
-//Lesson 10: Grading Students
+//Chapter 10: Grading Students
 List<int> output_10 = GradingStudents.Run(new() { 73, 67, 38, 33 });
 output_10.ForEach((item => Console.WriteLine(item)));
 
-//Lesson 11: Apple and Orange
+//Chapter 11: Apple and Orange
 
-//Lesson 12: Number Line Jumps
+//Chapter 12: Number Line Jumps
 string output_12 = NumberLineJumps.Run(0, 2, 5, 3); //output: "NO"
 
-//Lesson 13: Between Two Sets
+//Chapter 13: Between Two Sets
 
-//Lesson 14: Breaking the Records
+//Chapter 14: Breaking the Records
 List<int> output_14 = BreakingTheRecords.Run(new() { 10, 5, 20, 20, 4, 5, 2, 25, 1 });
 Console.WriteLine(String.Join(" ", output_14));
+
+//Chapter 15: Subarray Division
+int output_15 = SubarrayDivision.Run(new() { 1, 2, 1, 3, 2 }, 3, 2);
+Console.WriteLine(output_15);
 
 
