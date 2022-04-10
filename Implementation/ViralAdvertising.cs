aae3293b16387cf465e108057418852cbd76b81b
rdvnabay <ridvanabay@gmail.com>
2022-04-11 02:53:35 +0300

    Solution 33: Viral Advertising

---
+++ Implementation/ViralAdvertising.cs
@@ -0,0 +1,26 @@
+﻿using Implementation;
+
+public class ViralAdvertising
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"> 3 </param>
+    /// <returns> 9 </returns>
+    public static int Run(int n)
+    {
+        const int shareAdvertisingCount = 3;
+        int shared = 5;
+        int liked = 2;
+        int totalCumulative = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int cumulative = shared / liked;        
+            shared = cumulative * shareAdvertisingCount;
+            totalCumulative += cumulative;
+        }
+        return totalCumulative;
+    }
+}
+
