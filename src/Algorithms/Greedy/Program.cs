61958a566f53f3cb253724f383118b879a6abf77
Ridvan <ridvanabay@gmail.com>
2022-10-15 22:43:21 +0300

    Solution: Minimum Absolute Difference in an Array

---
+++ src/Algorithms/Greedy/Program.cs
@@ -0,0 +1,4 @@
+﻿using Greedy.Solutions;
+
+// Minimum Absolute Difference In An Array
+MinimumAbsoluteDifferenceInAnArray.Run(new List<int>() { 3, -7, 0 });
---
+++ src/Algorithms/Greedy/Solutions/MinimumAbsoluteDifferenceInAnArray.cs
@@ -0,0 +1,18 @@
+﻿namespace Greedy.Solutions;
+
+public class MinimumAbsoluteDifferenceInAnArray
+{
+    public static int Run(List<int> arr)
+    {
+        arr.Sort();
+        int minDiff = int.MaxValue;
+
+        for (int i = 0; i < arr.Count - 1; i++)
+        {
+            int currentDiff = Math.Abs(arr[i] - arr[i + 1]);
+            minDiff = Math.Min(currentDiff, minDiff);
+        }
+
+        return minDiff;
+    }
+}
