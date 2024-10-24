65f6493171cd0f88a11f04cd988986334d862aa9
Rıdvan Abay <ridvanabay@gmail.com>
2024-10-24 13:48:48 +0300

    Solution: Algorithms > Greedy > Permuting Two Arrays

--- src/Algorithms/Greedy/Program.cs
+++ src/Algorithms/Greedy/Program.cs
@@ -9,3 +9,5 @@ MarkAndToys.Run(new List<int>() { 1, 12, 5, 111, 200, 1000, 10 }, 50);
 MarcsCakewalk.Run(new List<int>() { 7, 4, 9, 6 });
 
 PriyankaAndToys.Run(new List<int>() { 1, 2, 3, 21, 7, 12, 14, 21 });
+
+PermutingTwoArrays.Run(10, new List<int>() { 2, 1, 3 }, new List<int>() { 7, 8, 9 });
---
+++ src/Algorithms/Greedy/Solutions/PermutingTwoArrays.cs
@@ -0,0 +1,18 @@
+﻿namespace Greedy.Solutions;
+
+public class PermutingTwoArrays
+{
+    public static string Run(int k, List<int> A, List<int> B)
+    {
+        var orderedA = A.OrderBy(x => x).ToList();
+        var orderedB = B.OrderByDescending(x => x).ToList();
+
+        for (int i = 0; i < A.Count; i++)
+        {
+            if (orderedA[i] + orderedB[i] < k)
+                return "NO";
+        }
+
+        return "YES";
+    }
+}
