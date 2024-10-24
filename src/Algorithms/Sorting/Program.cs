059e48a5081932cca90f8be6acd12373f07492bf
Rıdvan Abay <ridvanabay@gmail.com>
2024-10-24 19:00:29 +0300

    Solution: Algorithms > Sorting > Counting Sort 1

--- src/Algorithms/Sorting/Program.cs
+++ src/Algorithms/Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Sorting.Solutions;
 
-//Find the Median
 FindTheMedian.Run(new List<int> { 5, 3, 1, 2, 4 });
+
+CountingSort1.Run(new List<int>() { 1, 1, 3, 2, 1 });
---
+++ src/Algorithms/Sorting/Solutions/CountingSort1.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorting.Solutions;
+
+public class CountingSort1
+{
+    /// <param name="arr">an array of integers </param>
+    /// <returns>a frequency array</returns>
+    public static List<int> Run(List<int> arr)
+    {
+        List<int> result = new();
+        foreach (var number in Enumerable.Range(0, 100))
+        {
+            if (!arr.Any(x => x == number))
+            {
+                result.Add(0);
+                continue;
+            }
+
+            var count = arr.Count(x => x == number);
+            result.Add(count);
+        }
+
+        return result;
+    }
+}
