46b124746b1f973ea2fa12102513fc4cbf034ac0
Rıdvan Abay <ridvanabay@gmail.com>
2024-10-24 12:30:32 +0300

    Solution: Algorithms > Greedy > Marc's Cakewalk

--- src/Algorithms/Greedy/Program.cs
+++ src/Algorithms/Greedy/Program.cs
@@ -5,3 +5,5 @@ MinimumAbsoluteDifferenceInAnArray.Run(new List<int>() { 3, -7, 0 });
 
 // Mark and Toys
 MarkAndToys.Run(new List<int>() { 1, 12, 5, 111, 200, 1000, 10 }, 50);
+
+MarcsCakewalk.Run(new List<int>() { 7, 4, 9, 6 });
---
+++ src/Algorithms/Greedy/Solutions/MarcsCakewalk.cs
@@ -0,0 +1,20 @@
+﻿namespace Greedy.Solutions;
+
+public class MarcsCakewalk
+{
+    /// <param name="calorie">the calorie counts for each cupcake</param>
+    /// <returns>the minimum miles necessary</returns>
+    public static long Run(List<int> calorie)
+    {
+        var orderedCalorie = calorie.OrderByDescending(x => x).ToList();
+        long result = 0;
+
+        foreach (var index in Enumerable.Range(0, calorie.Count))
+        {
+            long calorieValue = Convert.ToInt64(orderedCalorie[index]);
+            result += calorieValue * Convert.ToInt64(Math.Pow(2, index));
+        }
+
+        return result;
+    }
+}
