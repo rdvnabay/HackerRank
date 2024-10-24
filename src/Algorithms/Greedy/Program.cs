dc12a0ac08b68b6e30e93d56e84d341ea8454592
Rıdvan Abay <ridvanabay@gmail.com>
2024-10-24 14:24:45 +0300

    Solution: Algorithms > Greedy > Jim and the Orders

--- src/Algorithms/Greedy/Program.cs
+++ src/Algorithms/Greedy/Program.cs
@@ -11,3 +11,12 @@ MarcsCakewalk.Run(new List<int>() { 7, 4, 9, 6 });
 PriyankaAndToys.Run(new List<int>() { 1, 2, 3, 21, 7, 12, 14, 21 });
 
 PermutingTwoArrays.Run(10, new List<int>() { 2, 1, 3 }, new List<int>() { 7, 8, 9 });
+
+JimAndTheOrders.Run(new List<List<int>>
+{
+    new List<int> { 8, 1 },
+    new List<int> { 4, 2 },
+    new List<int> { 5, 6 },
+    new List<int> { 3, 1 },
+    new List<int> { 4, 3 }
+});
---
+++ src/Algorithms/Greedy/Solutions/JimAndTheOrders.cs
@@ -0,0 +1,16 @@
+﻿namespace Greedy.Solutions;
+
+public class JimAndTheOrders
+{
+    public static List<int> Run(List<List<int>> orders)
+    {
+        Dictionary<int, int> dictionary = new();
+
+        for (int i = 0; i < orders.Count; i++)
+            dictionary.Add(i + 1, orders[i][0] + orders[i][1]);
+
+        var result = dictionary.OrderBy(x => x.Value).Select(x => x.Key).ToList();
+
+        return result;
+    }
+}
