cb49ab6db45d90cb5f1c40204e28c1b6b08595af
Rıdvan Abay <ridvanabay@gmail.com>
2024-10-25 11:33:25 +0300

    Solution: Algorithms > Sorting > Intro to Tutorial Challenges

--- src/Algorithms/Sorting/Program.cs
+++ src/Algorithms/Sorting/Program.cs
@@ -3,3 +3,5 @@
 FindTheMedian.Run(new List<int> { 5, 3, 1, 2, 4 });
 
 CountingSort1.Run(new List<int>() { 1, 1, 3, 2, 1 });
+
+IntroToTutorialChallenges.Run(4, new() { 1, 4, 5, 7, 9, 12 });
---
+++ src/Algorithms/Sorting/Solutions/IntroToTutorialChallenges.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorting.Solutions;
+
+public class IntroToTutorialChallenges
+{
+    /// <param name="V">a sorted array of integers </param>
+    /// <param name="arr">an integer to search for </param>
+    /// <returns>the index of in V arr</returns>
+    public static int Run(int V, List<int> arr)
+    {
+        return arr.FindIndex(x => x == V);
+    }
+}
